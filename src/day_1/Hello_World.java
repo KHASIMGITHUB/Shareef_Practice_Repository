24a168eb689610de8590b7b37648b9f2c4888504
khasi <khasi@192.168.1.34>
2026-01-06 11:42:01 +0530

    HelloWorld_Commit

---
+++ src/day_1/Hello_World.java
@@ -0,0 +1,22 @@
+package day_1;
+
+public class Hello_World {
+
+	  public void abc() {
+		  System.out.println("ABC");
+	  }
+	  public void xyz() {
+		  System.out.println("XYZ");
+	  
+	  }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Hello World");
+     Hello_World h1= new Hello_World ();
+     h1.xyz();
+     h1.abc();
+     
+	 }
+
+}
+
