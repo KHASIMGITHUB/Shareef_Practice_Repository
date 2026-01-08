08f7d9c183d4cc529e75854e101424885e7e7232
khasi <khasi@192.168.1.34>
2026-01-08 23:14:51 +0530

    operators

---
+++ src/day_3_Operators/BinaryOperator.java
@@ -0,0 +1,27 @@
+package day_3_Operators;
+
+public class BinaryOperator {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Relational - <,>,
+		 * Logical
+		 * Arithematic - + 
+		 */
+		System.out.println("Arathematic Operator");
+		int a=100;
+		int b=20;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+		System.out.println(a>b);
+		System.out.println(a==b);
+		
+	}
+	
+
+}
---
+++ src/day_3_Operators/UniryOperators.java
@@ -0,0 +1,19 @@
+package day_3_Operators;
+
+public class UniryOperators {
+
+	public static void main(String[] args) {
+		/*
+		 * pre Increment : ++a
+		 * Post increment :  a++
+		 * pre decrement : -- a
+		 * post decrement: a-- 
+		 * 
+		 */
+		Int a=12;
+		System.out.println();
+		
+				
+	}
+
+}
