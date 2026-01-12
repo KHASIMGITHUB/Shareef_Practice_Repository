0192298dc55bfb6c9498e1a9c0f94a5c3f3ee451
khasi <khasi@192.168.1.36>
2026-01-12 07:06:30 +0530

    Assignments

---
+++ src/assignments/Reminder_Practice.java
@@ -0,0 +1,15 @@
+package assignments;
+
+public class Reminder_Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+             int a=20; 
+             int b=100;
+             int reminder=a%b;
+             System.out.println("Reminder is:" + reminder);
+            		 
+  
+	}
+
+}
