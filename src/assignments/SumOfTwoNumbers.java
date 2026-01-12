260b500efd1b9ff9bd8724aa769b7bae2908acc7
khasi <khasi@192.168.1.36>
2026-01-12 12:19:58 +0530

    assignment

--- src/assignments/SumOfTwoNumbers.java
+++ src/assignments/SumOfTwoNumbers.java
@@ -4,13 +4,13 @@ public class SumOfTwoNumbers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 89;
+		int a = 88;
 		int b = 11;
 		int sum = a+b;
 		
 	System.out.println("Sum of two numbers:" + sum);
-		 
-		
+
+			
 	}
 
 }
