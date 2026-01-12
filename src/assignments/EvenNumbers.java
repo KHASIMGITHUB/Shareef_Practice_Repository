b5976073901cb78df7fdb70ad3c1de0f69a01de8
khasi <khasi@192.168.1.36>
2026-01-12 07:43:15 +0530

    Evennumber

---
+++ src/assignments/EvenNumbers.java
@@ -0,0 +1,23 @@
+package assignments;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 11;
+		int b = 2;
+		if(a%b==0) 
+		   {
+		      System.out.println("a is EvenNumber");			
+			}
+			else 
+		   {
+			  System.out.println("b is OddNumber");
+			}
+	}
+}
+		
+		
+	
+
+
